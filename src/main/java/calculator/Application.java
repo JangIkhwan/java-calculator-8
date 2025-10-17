ab5c689e4433af8d9461557357db6e428efbbced
JangIkhwan <bluensky0213@gmail.com>
2025-10-17 16:33:33 +0900

    feat(application): 덧셈기 입력값 파싱 구현
    - 기본 구분자를 이용하여 입력을 파싱
    - 입력에 커스텀 구분자가 있다면 이걸 구분자에 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,66 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    private static final String delimitChars = ",:";
+    private static final String delimitersToDoubleSlash = ".*+?^$()[]{}\\";
+    private static String customDelimiter = "";
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String line = Console.readLine();
+
+        // 앞에 커스텀 구분자가 있다면 구분자 변경
+        String removedPrefix = parsePrefix(line);
+
+        // 구분자로 구분하여서 입력을 정수 배열로 변환
+        int[] positiveIntegers = parsePositiveIntegers(removedPrefix);
+
+        
+    }
+
+    private static int[] parsePositiveIntegers(String line) {
+        String[] tokens = line.split("[" + delimitChars + customDelimiter + "]");
+        int[] positiveIntegers = new int[tokens.length];
+        for (int i = 0; i < positiveIntegers.length; i++) {
+            try {
+                System.out.println(tokens[i]);
+                positiveIntegers[i] = Integer.parseInt(tokens[i]);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("잘못된 형식의 입력입니다");
+            }
+
+            if (positiveIntegers[i] < 0) {
+                throw new IllegalArgumentException("입력은 0 또는 양수이어야 합니다");
+            }
+        }
+        return positiveIntegers;
+    }
+
+    private static String parsePrefix(String line){
+        String[] toks = line.split("\\\\n");
+        if(toks.length > 2){
+            throw new IllegalArgumentException("잘못된 형식의 입력입니다.");
+        }
+        if(toks.length == 1){
+            return toks[0];
+        }
+
+        String prefix = toks[0];
+        System.out.println(prefix);
+        if(!(prefix.startsWith("//") && prefix.length() == 3)){
+            throw new IllegalArgumentException("잘못된 형식의 입력입니다.");
+        }
+
+        String delimiter = prefix.substring(2, 3);
+        if(delimiter.equals("\\")){
+            delimiter = "\\\\";
+        }
+        else if(delimitersToDoubleSlash.contains(delimiter)){
+            delimiter = "\\" + delimiter;
+        }
+        customDelimiter = delimiter;
+
+        return toks[1];
     }
 }
