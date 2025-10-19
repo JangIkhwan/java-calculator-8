423def2bdb9271401ebb228dbb5ec4b8d4c5967c
JangIkhwan <bluensky0213@gmail.com>
2025-10-19 16:44:29 +0900

    feat(add-controller): 덧셈기 컨트롤러 구현
    - 도메인 모델인 adder가 입력과 출력인 뷰의 역할까지 수행하는 것은 적절하지 않다고 판단해여서 뷰와 모델을 제어하는 컨트롤러를 구현

---
+++ src/main/java/calculator/controller/AddController.java
@@ -0,0 +1,24 @@
+package calculator.controller;
+
+import calculator.Adder;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
+public class AddController {
+    private final Adder adder;
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public AddController(Adder adder, InputView inputView, OutputView outputView){
+        this.adder = adder;
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run(){
+        int[] positiveIntegers = inputView.inputPositiveIntegers();
+        long result = adder.add(positiveIntegers);
+        outputView.printResult(result);
+    }
+
+}
