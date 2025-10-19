63310ae8293a94371fb42ea847bc24d7e3fb6ec8
JangIkhwan <bluensky0213@gmail.com>
2025-10-19 16:50:00 +0900

    fix(application): main 메서드 수정
    - 애플리케이션에 MVC 패턴을 적용함에 따라 애플리케이션 실행 시에 컨트롤러를 실행하도록 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,9 +1,16 @@
 package calculator;
 
+import calculator.controller.AddController;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         InputParser parser = new InputParser();
-        Adder adder = new Adder(parser);
-        adder.execute();
+        Adder adder = new Adder();
+        InputView inputView = new InputView(parser);
+        OutputView outputView = new OutputView();
+        AddController controller = new AddController(adder, inputView, outputView);
+        controller.run();
     }
 }
